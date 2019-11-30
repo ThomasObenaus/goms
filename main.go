0ee590d8d90d305660c0627c4809a9b46147503a
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-11-30 17:07:59 +0100

    .Update added buildinfo

--- main.go
+++ main.go
@@ -2,9 +2,60 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/thomasobenaus/go-ms-poc/api"
+	"github.com/thomasobenaus/go-ms-poc/logging"
 )
 
-func main() {
-	fmt.Printf("Hello goms %s\n", "sdf")
+var version string
+var buildTime string
+var revision string
+var branch string
 
+func main() {
+
+	buildInfo := BuildInfo{
+		Version:   version,
+		BuildTime: buildTime,
+		Revision:  revision,
+		Branch:    branch,
+	}
+	buildInfo.Print(fmt.Printf)
+
+	port := 12000
+	// set up logging
+	loggingFactory := logging.New(false, false, false)
+	loggerMain := loggingFactory.NewNamedLogger("goms")
+
+	api := api.New(port, api.WithLogger(loggingFactory.NewNamedLogger("goms.api")))
+
+	// Register build info end-point
+	api.Router.GET(PathBuildInfo, buildInfo.BuildInfo)
+	loggerMain.Info().Str("end-point", "build info").Msgf("Build Info end-point set up at %s", PathBuildInfo)
+
+	// Install signal handler for shutdown
+	shutDownChan := make(chan os.Signal, 1)
+	signal.Notify(shutDownChan, syscall.SIGINT, syscall.SIGTERM)
+	go shutdownHandler(shutDownChan, api, loggerMain)
+
+	// start api
+	api.Run()
+	// wait for completion
+	api.Join()
+
+	loggerMain.Info().Msg("Shutdown successfully completed")
+	os.Exit(0)
+}
+
+// shutdownHandler handler that shuts down the running components in case
+// a signal was sent on the given channel
+func shutdownHandler(shutdownChan <-chan os.Signal, api *api.API, logger zerolog.Logger) {
+	s := <-shutdownChan
+	logger.Info().Msgf("Received %v. Shutting down...", s)
+
+	api.Stop()
 }
---
+++ routes.go
@@ -0,0 +1,15 @@
+package main
+
+const (
+	// PathHealth is the url path for health end-point
+	PathHealth = "/health"
+
+	// PathMetrics path for the metrics end-point
+	PathMetrics = "/metrics"
+
+	// PathBuildInfo specifies the path where to get the build information about sokar
+	PathBuildInfo = "/api/build"
+
+	// PathConfig specifies the path where to get the config information used by sokar
+	PathConfig = "/api/config"
+)
