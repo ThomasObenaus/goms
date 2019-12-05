759cfce096ef455a4d0bf5eb41971f1bacb02e96
Thomas Obenaus <obenaus.thomas@gmail.com>
2019-12-05 15:49:20 +0100

    .Update metrics ep added

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/auth0-community/go-auth0 v1.0.0
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.2.0
+	github.com/prometheus/client_golang v1.2.1
 	github.com/rs/zerolog v1.17.2
 	github.com/stretchr/testify v1.4.0
 	gopkg.in/square/go-jose.v2 v2.1.7
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/thomasobenaus/goms/api"
 	"github.com/thomasobenaus/goms/auth"
@@ -38,21 +39,18 @@ func main() {
 	authHandler := auth.New("http://localhost:8180/auth/realms/gocloak/protocol/openid-connect/certs",
 		"goms", "http://localhost:8180/auth/realms/gocloak", auth.WithLogger(loggingFactory.NewNamedLogger("auth")))
 
-	companyCtrl, err := setupControllers()
-	if err != nil {
-		loggerMain.Fatal().Err(err).Msg("Failed to create controllers.")
-	}
-	api.GET(PathCompany, authHandler.HandleSecure(companyCtrl.GetCompany, auth.HasRealmRole("goms_role")))
-	loggerMain.Info().Str("end-point", "company").Msgf("company end-point set up at %s", PathCompany)
-	api.GET(PathCompaniesAll, authHandler.HandleSecure(companyCtrl.GetCompanies, auth.HasRealmRole("goms_role")))
-	loggerMain.Info().Str("end-point", "companies all").Msgf("companies all end-point set up at %s", PathCompaniesAll)
-
-	api.GET(PathCompanies, authHandler.HandleSecure(companyCtrl.GetCompaniesWithUsers, auth.HasRealmRole("goms_role")))
-	loggerMain.Info().Str("end-point", "companies").Msgf("companies end-point set up at %s", PathCompanies)
+	// Register metrics handler
+	api.Router.Handler("GET", PathMetrics, promhttp.Handler())
+	loggerMain.Info().Str("end-point", "metrics").Msgf("Metrics end-point set up at %s", PathMetrics)
 
 	// Register build info end-point
 	api.GET(PathBuildInfo, buildInfo.BuildInfo)
 	loggerMain.Info().Str("end-point", "build info").Msgf("Build Info end-point set up at %s", PathBuildInfo)
+
+	err := setupControllers(api, authHandler, loggerMain, "goms_role")
+	if err != nil {
+		loggerMain.Fatal().Err(err).Msg("Failed to create controllers.")
+	}
 
 	// Install signal handler for shutdown
 	shutDownChan := make(chan os.Signal, 1)
@@ -77,20 +75,29 @@ func shutdownHandler(shutdownChan <-chan os.Signal, api *api.API, logger zerolog
 	api.Stop()
 }
 
-func setupControllers() (*controller.CompanyController, error) {
+func setupControllers(api *api.API, authHandler *auth.Auth, logger zerolog.Logger, requiredRole string) error {
 
 	// open db connection
 	dbconn, err := postgres.New("goms", "goms", "goms")
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	if err := dbconn.Ping(); err != nil {
-		return nil, err
+		return err
 	}
 	companyRepo := postgres.NewPGCompanyRepo(dbconn)
 
 	companyController := controller.New(companyRepo)
 
-	return &companyController, nil
+	api.GET(PathCompany, authHandler.HandleSecure(companyController.GetCompany, auth.HasRealmRole(requiredRole)))
+	logger.Info().Str("end-point", "company").Msgf("company end-point set up at %s", PathCompany)
+
+	api.GET(PathCompaniesAll, authHandler.HandleSecure(companyController.GetCompanies, auth.HasRealmRole(requiredRole)))
+	logger.Info().Str("end-point", "companies all").Msgf("companies all end-point set up at %s", PathCompaniesAll)
+
+	api.GET(PathCompanies, authHandler.HandleSecure(companyController.GetCompaniesWithUsers, auth.HasRealmRole(requiredRole)))
+	logger.Info().Str("end-point", "companies").Msgf("companies end-point set up at %s", PathCompanies)
+
+	return nil
 }
